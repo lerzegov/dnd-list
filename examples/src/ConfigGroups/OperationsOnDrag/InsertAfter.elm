9febd97cf69d3c8493b57e0c12b324a93e0ec297
Cedd Burge <ceddlyburge@gmail.com>
2024-04-17 13:56:41 +0100

    Fix touch action for groups operations on drop

--- examples/src/ConfigGroups/OperationsOnDrag/InsertAfter.elm
+++ examples/src/ConfigGroups/OperationsOnDrag/InsertAfter.elm
@@ -189,7 +189,7 @@ update message model =
 view : Model -> Html.Html Msg
 view model =
     Html.section
-        (Html.Events.onMouseDown ResetColors :: Html.Attributes.style "touch-action" "none" :: sectionStyles)
+        (Html.Events.onMouseDown ResetColors :: sectionStyles)
         [ groupView model 1
         , groupView model 2
         , groupView model 3
@@ -220,38 +220,38 @@ itemView model offset localIndex { group, value, color } =
         ( Just { dragIndex, dropIndex }, Just dragItem ) ->
             if value == "" && group /= dragItem.group then
                 Html.div
-                    (Html.Attributes.id itemId :: auxiliaryItemStyles ++ system.dropEvents globalIndex itemId)
+                    (Html.Attributes.id itemId :: Html.Attributes.style "touch-action" "none" :: auxiliaryItemStyles ++ system.dropEvents globalIndex itemId)
                     []
 
             else if value == "" && group == dragItem.group then
                 Html.div
-                    (Html.Attributes.id itemId :: auxiliaryItemStyles)
+                    (Html.Attributes.id itemId :: Html.Attributes.style "touch-action" "none" :: auxiliaryItemStyles)
                     []
 
             else if globalIndex /= dragIndex && globalIndex /= dropIndex then
                 Html.div
-                    (Html.Attributes.id itemId :: itemStyles color ++ system.dropEvents globalIndex itemId)
+                    (Html.Attributes.id itemId :: Html.Attributes.style "touch-action" "none" :: itemStyles color ++ system.dropEvents globalIndex itemId)
                     [ Html.text value ]
 
             else if globalIndex /= dragIndex && globalIndex == dropIndex then
                 Html.div
-                    (Html.Attributes.id itemId :: itemStyles dropColor ++ system.dropEvents globalIndex itemId)
+                    (Html.Attributes.id itemId :: Html.Attributes.style "touch-action" "none" :: itemStyles dropColor ++ system.dropEvents globalIndex itemId)
                     [ Html.text value ]
 
             else
                 Html.div
-                    (Html.Attributes.id itemId :: itemStyles dropColor)
+                    (Html.Attributes.id itemId :: Html.Attributes.style "touch-action" "none" :: itemStyles dropColor)
                     []
 
         _ ->
             if value == "" then
                 Html.div
-                    (Html.Attributes.id itemId :: auxiliaryItemStyles)
+                    (Html.Attributes.id itemId :: Html.Attributes.style "touch-action" "none" :: auxiliaryItemStyles)
                     []
 
             else
                 Html.div
-                    (Html.Attributes.id itemId :: itemStyles color ++ system.dragEvents globalIndex itemId)
+                    (Html.Attributes.id itemId :: Html.Attributes.style "touch-action" "none" :: itemStyles color ++ system.dragEvents globalIndex itemId)
                     [ Html.text value ]
 
 
